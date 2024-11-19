b35833641b84f4ac1403effe77e35193cdbe52e7
Humayun Wahla <humayunwahla55@gmail.com>
2024-11-19 16:43:11 +0500

    first commit

---
+++ app/footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Footer() {
+    return (
+      <div className='flex flex-row text-center mx-auto py-12 bg-stone-100'>
+        <div className='justify-center mx-auto'>
+         
+          <h1 className='text-lg font-bold '>Home</h1>
+         
+        </div>
+        <div className='justify-center mx-auto'>
+         
+          <h1 className='text-lg font-bold '>Users</h1>
+         
+        </div>
+      </div>
+    )
+  }
+  
+  export default Footer
---
+++ app/header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Header() {
+    return (
+      <div className='center  mx-auto py-12 bg-stone-100'>
+          <div className='flex justify-center gap-6'>
+  
+        <div>
+          
+          <h1 className='text-lg font-bold '>Home</h1>
+          
+        </div>
+        <div>
+          
+          <h1 className='text-lg font-bold '>Users</h1>
+         
+        </div>
+          </div>
+      </div>
+    )
+  }
+  
+  export default Header
